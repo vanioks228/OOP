2a645727e1cb94b28b032faff9d2e0a04bf89869
vanioks <63503870+vanioks228@users.noreply.github.com>
2021-09-25 14:43:31 +0700

    Corrected version

---
+++ Function extremum/Function extremum/GoldenSection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_extremum
+{
+    class GoldenSection
+    {
+        private const double PHI = 1.6180339887498949;
+
+        /// <summary>
+        /// Метод поиска минимума на заданном отрезке.
+        /// </summary>
+        /// <param name="func">Заданная функция.</param>
+        /// <param name="a">Начало отрезка.</param>
+        /// <param name="b">Конец отрезка.</param>
+        /// <param name="e">Точность.</param>
+        /// <returns></returns>
+
+        public double GetMax(Func<double, double> func, double a, double b, double e)
+        {
+            double x1;
+            double x2;
+
+            while (true)
+            {
+                x1 = b - (b - a) / PHI;
+                x2 = a + (b - a) / PHI;
+
+                if (func(x1) <= func(x2))
+                    a = x1;
+                else
+                    b=x2;
+
+                if (Math.Abs(b-a)<e)
+                    break;
+            }
+        return (a + b) / 2;
+        }
+
+        public double GetMin(Func<double, double> func, double a, double b, double e)
+        {
+            double x1;
+            double x2;
+
+            while (true)
+            {
+                x1 = b - (b - a) / PHI;
+                x2 = a + (b - a) / PHI;
+
+                if (func(x1) >= func(x2))
+                    a = x1;
+                else
+                    b = x2;
+
+                if (Math.Abs(b - a) < e)
+                    break;
+            }
+            return (a + b) / 2;
+        }
+    }
+}
---
+++ Function extremum/Function extremum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Function_extremum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoldenSection gs = new GoldenSection();
+            double max = gs.GetMax(Function, -8.0, 0.0, 0.0001);
+            double min = gs.GetMin(Function, -2.0, 2.0, 0.0001);
+            Console.WriteLine($"Max:{max}, Min:{min}");
+        }
+        static double Function(double x) => Math.Pow(x + 1, 3) + 5 * Math.Pow(x, 2);
+    }
+}
