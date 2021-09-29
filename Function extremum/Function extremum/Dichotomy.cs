847a9cab0ca17a6b6dd6413f0aa2c2766e9c8235
vanioks <63503870+vanioks228@users.noreply.github.com>
2021-09-29 14:07:46 +0700

    Add Dichotomy

---
+++ Function extremum/Function extremum/Dichotomy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_extremum
+{
+    class Dichotomy
+    {
+        /// <summary>
+        /// Метод поиска минимума фунции на заданном отрезке
+        /// </summary>
+        /// <param name="func">Заданная функция.</param>
+        /// <param name="a">Начало отрезка.</param>
+        /// <param name="b">Конец отрезка.</param>
+        /// <param name="e">Точность.</param>
+        /// <returns></returns>
+        public double ExtrMin(Func<double, double> func, double a, double b, double e)
+        {
+            double x;
+
+            while (true)
+            {
+                x = (a + b) / 2;
+
+                if (func(x - e) <= func(x + e))
+                    b = x;
+                else
+                    a = x;
+
+                if (Math.Abs(b - a) <= e)
+                    return x;
+            }
+        }
+
+        /// <summary>
+        /// Метод поиска максимума фунции на заданном отрезке
+        /// </summary>
+        /// <param name="func">Заданная функция.</param>
+        /// <param name="a">Начало отрезка.</param>
+        /// <param name="b">Конец отрезка.</param>
+        /// <param name="e">Точность.</param>
+        /// <returns></returns>
+        public double ExtrMax(Func<double, double> func, double a, double b, double e)
+        {
+            double x;
+
+            while (true)
+            {
+                x = (a + b) / 2;
+
+                if (func(x - e) >= func(x + e))
+                    b = x;
+                else
+                    a = x;
+
+                if (Math.Abs(b - a) <= e)
+                    return x;
+            }
+        }
+    }
+}
+
--- Function extremum/Function extremum/Program.cs
+++ Function extremum/Function extremum/Program.cs
@@ -6,10 +6,24 @@ namespace Function_extremum
     {
         static void Main(string[] args)
         {
-            GoldenSection gs = new GoldenSection();
-            double max = gs.GetMax(Function, -8.0, 0.0, 0.0001);
-            double min = gs.GetMin(Function, -2.0, 2.0, 0.0001);
-            Console.WriteLine($"Max:{max}, Min:{min}");
+            Dichotomy dicht = new();
+            GoldenSection gs = new();
+
+            Console.WriteLine("Метод золотого сечния:");
+            Console.WriteLine("Интервал: [-2;2]");
+            Console.WriteLine("Точка минимума функции:x=" + gs.GetMin(Function, -2, 2, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + gs.GetMax(Function, -2, 2, 0.001));
+            Console.WriteLine("Интервал: [0;8]");
+            Console.WriteLine("Точка минимума функции:x=" + gs.GetMin(Function, 0, 8, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + gs.GetMax(Function, 0, 8, 0.001));
+
+            Console.WriteLine("\nМетод дехотомии:");
+            Console.WriteLine("Интервал: [-2;2]");
+            Console.WriteLine("Точка минимума функции:x=" + dicht.ExtrMin(Function, -2, 2, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + dicht.ExtrMax(Function, -2, 2, 0.001));
+            Console.WriteLine("Интервал: [0;8]");
+            Console.WriteLine("Точка минимума функции:x=" + dicht.ExtrMin(Function, 0, 8, 0.001));
+            Console.WriteLine("Точка максимума функции:x=" + dicht.ExtrMax(Function, 0, 8, 0.001));
         }
         static double Function(double x) => Math.Pow(x + 1, 3) + 5 * Math.Pow(x, 2);
     }
